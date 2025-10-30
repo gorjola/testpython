d14c64105a0c85f12b86437f3fa459c26915273b
Giorgi Gorjoladze <ggipian@gmail.com>
2025-10-30 14:37:45 +0400

    first com

---
+++ homework1.py
@@ -0,0 +1,30 @@
+"""
+first
+"""
+numberOne =5
+numberTwo =10
+numberTree =15
+sum=numberOne+numberTwo+numberTree
+print("sum is: ",sum)
+
+"""
+second
+"""
+
+celsius= 27
+fahrenheit=(celsius * 9/5)+32
+print("celsius: ",celsius)
+print("fahrenheit: ",fahrenheit)
+
+"""
+third
+"""
+side = float(input("შეიყვანეთ კუბის გვერდის სიგრძე: "))
+
+
+surfaceArea = 6 * (side ** 2)
+volume = side ** 3
+
+print("კუბის ზედაპირის ფართობი:", surfaceArea)
+print("კუბის მოცულობა:", volume)
+
